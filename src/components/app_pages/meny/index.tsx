593f5ab9eef0a47bf39fc3d8e3e1c7daddce061c
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2023-09-28 10:29:09 +0200

    Added signout function

--- src/components/app_pages/meny/index.tsx
+++ src/components/app_pages/meny/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import type { HeadFC, PageProps } from "gatsby";
-import Layout from "../../layout";
+import { signOut } from "firebase/auth";
 import FixedHeader from "../../FixedHeader";
 import { MenuButtonInternal, MenuButtonExternal } from "./MenuButtons";
 import { PrimaryButton } from "../../buttons";
+
+import { auth } from "../../../firebase";
 
 const AppMenuPage: React.FC<PageProps> = () => {
   return (
@@ -12,17 +14,28 @@ const AppMenuPage: React.FC<PageProps> = () => {
         <h1 className="mx-auto text-base text-white">Meny</h1>
       </FixedHeader>
       <main className="mt-20">
-         <ul className="max-w-screen-sm mx-auto">
-            <li><MenuButtonInternal href="/app/galleri" title="Galleri" icon="gallery"/></li>
-            <li><MenuButtonInternal href="/app/om" title="Om" icon="information"/></li>
-            <li><MenuButtonExternal href="/app/om" title="Admin" icon="admin"/></li>
-         </ul>
-         <div className="flex justify-center mt-20">
-            <PrimaryButton 
-               text={"Logga ut"}
-               actionText={"Klicka här för att logga ut från portalen"}
+        <ul className="max-w-screen-sm mx-auto">
+          <li>
+            <MenuButtonInternal
+              href="/app/galleri"
+              title="Galleri"
+              icon="gallery"
             />
-         </div>
+          </li>
+          <li>
+            <MenuButtonInternal href="/app/om" title="Om" icon="information" />
+          </li>
+          <li>
+            <MenuButtonExternal href="/app/om" title="Admin" icon="admin" />
+          </li>
+        </ul>
+        <div className="flex justify-center mt-20">
+          <PrimaryButton
+            onClick={() => signOut(auth)}
+            text={"Logga ut"}
+            actionText={"Klicka här för att logga ut från portalen"}
+          />
+        </div>
       </main>
     </>
   );
